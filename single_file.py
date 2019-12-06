380f29050a62154fd627068424760d936474722d
Ravi Ranjan <raviranjan.amu@gmail.com>
2019-12-06 16:26:53 +0530

    Update single_file.py

--- single_file.py
+++ single_file.py
@@ -1,5 +1,7 @@
 def main():
-  print("Hello World!")
+  print(" Top 10 Skills with Salary ranges –")
+  print(" Top 10 Job categories with above query")
+  print("salary ranges based on job category and years of experience")
   
 if __name__== "__main__":
   main()
