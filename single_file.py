b3b0f798a90e4e1c06a15341967bb816edd90fcd
Ravi Ranjan <raviranjan@vmware.com>
2019-12-16 21:16:32 +0530

    adding salary.piynb

--- single_file.py
+++
@@ -1,7 +0,0 @@
-def main():
-  print(" Top 10 Skills with Salary ranges –")
-  print(" Top 10 Job categories with above query")
-  print("salary ranges based on job category and years of experience")
-  
-if __name__== "__main__":
-  main()
